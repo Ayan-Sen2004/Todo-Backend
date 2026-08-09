ca864edb00ba92e826cd8f2a2462407db4096d8d
Hii-Ayan933 <ayansen727@gmail.com>
2026-08-09 16:29:24 +0530

    feat : hashing issue solve

--- app/utils/hash.py
+++ app/utils/hash.py
@@ -1,23 +1,20 @@
-import bcrypt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
 
 def hash_password(password: str) -> str:
-    password_bytes = password.encode("utf-8")
-
-    hashed = bcrypt.hashpw(
-        password_bytes,
-        bcrypt.gensalt()
-    )
-
-    return hashed.decode("utf-8")
+    return pwd_context.hash(password)
 
 
 def verify_password(
     plain_password: str,
     hashed_password: str
 ) -> bool:
-
-    return bcrypt.checkpw(
-        plain_password.encode("utf-8"),
-        hashed_password.encode("utf-8")
+    return pwd_context.verify(
+        plain_password,
+        hashed_password
     )
