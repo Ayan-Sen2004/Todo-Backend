682f613faeb4082e65143f7ed5d86715d84e980a
Hii-Ayan933 <ayansen727@gmail.com>
2026-08-09 15:08:40 +0530

    feat : ci cd pipeline fixed

--- app/config.py
+++ app/config.py
@@ -8,6 +8,10 @@ DATABASE_NAME = os.getenv("DATABASE_NAME")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
+
 ACCESS_TOKEN_EXPIRE_MINUTES = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30")
+    os.getenv(
+        "ACCESS_TOKEN_EXPIRE_MINUTES",
+        "30"
+    )
 )
